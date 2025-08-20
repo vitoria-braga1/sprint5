c68669ee0794dc915ba8681d2af15a3847c3d2b7
vitoria-braga1 <Vb69949@gmail.com>
2025-08-20 08:31:19 -0300

    atulizaçao da alteracoes do projeto 5.1

--- app.py
+++ app.py
@@ -4,7 +4,30 @@ from pathlib import Path
 import streamlit as st
 
 df = pd.read_csv(Path("Data/vehicles.csv"))
-fig = px.histogram(df, x = 'days_listed')
+
+df["model_year"] = df["model_year"].fillna(0).astype(int)
 
 st.title("projeto 5-teste")
-st.plotly_chart(fig)
+
+
+tipos = st.multiselect(
+    "Selecione o(s) tipo(s) de carro:",
+    df["type"].dropna().unique(),
+    default=list(df["type"].dropna().unique())
+)
+df_filtrado = df[df["type"].isin(tipos)]
+
+if not df_filtrado.empty:
+    df_tipo = df_filtrado.groupby("type").size().reset_index(name="count")
+
+    fig = px.bar(
+        df_tipo,
+        x="type",
+        y="count",
+        title="📊 Quantidade de Carros por Tipo",
+        labels={"type": "Tipo de Carro", "count": "Quantidade"}
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+else:
+    st.warning("⚠️ Nenhum carro encontrado para esse(s) tipo(s).")
